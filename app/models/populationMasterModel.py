c93becf629c489dea78af9d48bb531ce317da1d0
Sudharsan2618 <105963347+Sudharsan2618@users.noreply.github.com>
2025-03-23 13:20:26 +0530

    country code with percentage

--- app/models/populationMasterModel.py
+++ app/models/populationMasterModel.py
@@ -4,24 +4,22 @@ from datetime import datetime
 def get_population_data_for_current_year(conn):
     current_year = datetime.now().year
     query = """
-        SELECT 
-            cc.country_name, 
-            cc.country_iso_code, 
-            phm.country_share_of_world_population 
-        FROM 
-            sidi_population_master.country_code AS cc
-        LEFT JOIN 
-            sidi_population_master.country_master AS cm 
-        ON 
-            cm.country_name = cc.country_name
-        LEFT JOIN 
-            sidi_population_master.population_historical_master AS phm 
-        ON 
-            phm.country_id = cm.country_id
-        WHERE 
-            cc.country_name = cm.country_name
-        AND 
-            phm.year = 2024
+     
+SELECT 
+    cc.country_name, 
+    cc.country_iso_code, 
+    COALESCE(phm.country_share_of_world_population, 0) AS country_share_of_world_population
+FROM 
+    sidi_population_master.country_code AS cc
+LEFT JOIN 
+    sidi_population_master.country_master AS cm 
+ON 
+    cm.country_name = cc.country_name
+LEFT JOIN 
+    sidi_population_master.population_historical_master AS phm 
+ON 
+    phm.country_id = cm.country_id 
+    AND phm.year = 2024;
     """
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute(query, (current_year,))
